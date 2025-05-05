e2e375cc105ace0afc679c2fcc844afc097e69a9
Igor Ricardo <122498648+IgorRicardo1@users.noreply.github.com>
2025-05-05 14:30:54 -0300

    commit para vinculo do computador

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,45 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function Home() {
+  const router = useRouter();
+
+  const handlePress = () => {
+    router.push('/cadastro'); // Ou '/cadastro' se quiser ir direto
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo ao AthusApp!</Text>
+
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>Iniciar Cadastro</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#d2691e',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
